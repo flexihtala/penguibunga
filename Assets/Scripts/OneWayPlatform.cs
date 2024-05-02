8900bac058e5399c57f1acc44322bab277878856
Ovchinnikov Danila <danovch24@gmail.com>
2024-05-02 21:13:34 +0200

    update-oneway-platform

---
+++ Assets/Scripts/OneWayPlatform.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayPlatform : MonoBehaviour
+{
+    public PlatformEffector2D platformEffector;
+    private bool isTriggered;
+    // Start is called before the first frame update
+    
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            if (isTriggered && Input.GetKeyDown(KeyCode.S))
+            {
+                platformEffector.rotationalOffset = 180;
+            }
+        }
+    }
+    
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            isTriggered = true;
+        }
+    }
+    
+    
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            isTriggered = false;
+            platformEffector.rotationalOffset = 0;
+        }
+    }
+}
