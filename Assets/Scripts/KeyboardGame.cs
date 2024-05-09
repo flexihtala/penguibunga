d25662a0fea5a1bb96f41c2f517071e299743a9d
flexihtala <flexihtala@gmail.com>
2024-05-10 01:45:52 +0500

    Fixed Keyboard game bugs

---
+++ Assets/Scripts/KeyboardGame.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardGame : MonoBehaviour
+{
+    public GameObject game;
+    private bool isTriggered;
+
+    private void Awake()
+    {
+        game.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (isTriggered && Input.GetKeyDown(KeyCode.E))
+            game.SetActive(true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && other.GetComponent<Player>().penguinName == PenguinNames.Kawazaki)
+        {
+            isTriggered = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isTriggered = false;
+            game.SetActive(false);
+        }
+    }
+}
