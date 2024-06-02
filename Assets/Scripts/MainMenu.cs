98383f31be59ee1ca18ac20fa373e2d5cd542c14
Vinter-mach <vintermach@gmail.com>
2024-06-02 13:35:16 +0500

    First_try

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,9 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
     public void PlayGame()
     {
+        //StartCoroutine(WaitTime());
         SceneManager.LoadScene("Game");
+    }
+    
+    private IEnumerator WaitTime()
+    {
+        animator.SetBool("IsStartAnimation", true);
+        yield return new WaitForSeconds(10);
     }
 
     public void ExitGame()
